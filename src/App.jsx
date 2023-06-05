c2d5006628a1f18fe4e702defe44dbc0551567af
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-06-05 15:58:10 -0500

    custom-templates-into-components

--- src/App.jsx
+++ src/App.jsx
@@ -9,10 +9,18 @@ function App() {
       </header>
       <img class='rounded-2xl' src={banner} alt="site banner" />
       <div class='grid grid-cols-4 gap-10 my-4'>
-      {/* <Card title='Ninja tee'/> */}
-      <Card title='Jacket samurai' rounded={true} flat={false}/>
-      <Card title='shuriken pants'rounded={false} flat={true}/>
-
+      {/* <Card /> */}
+      <Card rounded={true} flat={false}>
+        <h2>Ninja Tee, Black</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptate ad illum ipsa cumque, iure eveniet!</p>
+        <button class='btn'>View</button>
+      </Card>
+      <Card rounded={false} flat={true}>
+      <h2>Ninja Tee, White</h2>
+        <button class='btn'>Click me!</button>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptate ad illum ipsa cumque, iure eveniet!</p>
+        <p>Only S/59</p>
+      </Card>
       </div>
     </div>
   );
