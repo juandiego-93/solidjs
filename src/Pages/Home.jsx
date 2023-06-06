f0afc48a863ba82700612ab9512f3e088c947c35
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-06-05 22:34:14 -0500

    final-clase

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,15 @@
+import { createResource } from "solid-js"
 import Card from "../Components/Card"
 
+async function fetchProducts() {
+    const res = await fetch('http://localhost:4000/products')
+    return res.json()
+}
+
 export default function Home() {
+    const [products] = createResource(fetchProducts) 
+
+
     return (
         <div class='grid grid-cols-4 gap-10 my-4'>
         {/* <Card /> */}
@@ -15,6 +24,8 @@ export default function Home() {
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci voluptate ad illum ipsa cumque, iure eveniet!</p>
           <p>Only S/59</p>
         </Card>
+
+        <p>{console.log(products(), products.loading)}</p>
         </div>
     )
 }
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import { render } from 'solid-js/web';
 import { Router } from '@solidjs/router';
 import './index.css';
 import App from './App';
+import 'material-icons/iconfont/material-icons.css'
 
 const root = document.getElementById('root');
 
