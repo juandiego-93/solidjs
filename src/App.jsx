475b3c089471175d6ea2737181e60eeaf9d6a0ee
Juan Diego Salvador <juandiegosalvador@gmail.com>
2023-06-05 19:49:34 -0500

    se agrego el router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
+import { createSignal } from 'solid-js';
 import Card from './Components/Card';
 import banner from './assets/banner.png'
 
 function App() {
+  const [darkTheme, setDarkTheme] = createSignal(false)
+
+  const toggleTheme = () =>{
+    setDarkTheme(!darkTheme())
+  }
+
   return (
     <div class='container m-auto '>
-      <header>
+      <header
+        class='my-4 p-2 text-xl flex items-center gap-4 rounded-lg'
+        classList={{ 'bg-neutral-900': darkTheme(), 'text-white': darkTheme() }}>
+        <span
+          class='material-symbols-outlined cursor-pointer'
+          onClick={toggleTheme}>
+          light_mode
+        </span>
         <h1> Ninja Merch</h1>
       </header>
       <img class='rounded-2xl' src={banner} alt="site banner" />
